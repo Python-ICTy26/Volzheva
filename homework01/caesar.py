745fabc076f4dd2366833e7f544b46263a8c8635
Maria <47611067+Volzheva@users.noreply.github.com>
2022-10-18 17:59:04 +0300

    Rewrite to correct repo

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -2,9 +2,9 @@ import typing as tp
 
 
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
+    alfavit = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     """
     Encrypts plaintext using a Caesar cipher.
-
     >>> encrypt_caesar("PYTHON")
     'SBWKRQ'
     >>> encrypt_caesar("python")
@@ -19,9 +19,9 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     for i in plaintext:
         if i in alfavit:
             if i.lower() == i:
-                start = ord('a')
+                start = ord("a")
             else:
-                start = ord('A')
+                start = ord("A")
             ciphertext += chr(start + (ord(i) + shift - start) % num_letters)
         else:
             ciphertext += i
@@ -31,7 +31,6 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     """
     Decrypts a ciphertext using a Caesar cipher.
-
     >>> decrypt_caesar("SBWKRQ")
     'PYTHON'
     >>> decrypt_caesar("sbwkrq")
@@ -41,24 +40,5 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     >>> decrypt_caesar("")
     ''
     """
-    plaintext = ""
-    num_letters = 26
-    for i in ciphertext:
-        if i in alfavit:
-            if i.lower() == i:
-                start = ord('a')
-            else:
-                start = ord('A')
-            plaintext += chr(start + (ord(i) - shift - start) % num_letters)
-        else:
-            plaintext += i
-    return plaintext
 
-
-def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
-    """
-    Brute force breaking a Caesar cipher.
-    """
-    best_shift = 0
-    # PUT YOUR CODE HERE
-    return best_shift
+    return encrypt_caesar(ciphertext, -shift)
