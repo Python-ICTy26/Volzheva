4fc38d61b5edbbbecac0b5daf2566fc221d40125
Maria <47611067+Volzheva@users.noreply.github.com>
2022-12-07 00:05:16 +0300

    final commit

--- homework06/hackernews.py
+++ homework06/hackernews.py
@@ -33,6 +33,20 @@ def add_label():
     s.commit()
     redirect("/news")
 
+@route("/add_classify_label/")
+def add_label():
+    # 1. Получить значения параметров label и id из GET-запроса
+    # 2. Получить запись из БД с соответствующим id (такая запись только одна!)
+    # 3. Изменить значение метки записи на значение label
+    # 4. Сохранить результат в БД
+    args = parse_qs(request.query_string)
+    s = session()
+    s.query(News). \
+        filter(News.id == int(args['id'][0])). \
+        update({'label': str(args['label'][0])})
+    s.commit()
+    redirect("/classify")
+
 
 @route("/update")
 def update_news():
@@ -56,10 +70,39 @@ def update_news():
     redirect("/news")
 
 
-@route("/classify")
-def classify_news():
-    # PUT YOUR CODE HERE
-    pass
+@route("/recommendations")
+def recommendations():
+    # 1. Получить список неразмеченных новостей из БД
+    # 2. Получить прогнозы для каждой новости
+    # 3. Вывести ранжированную таблицу с новостями
+    #строим таблицу вероятностей
+    s = session()
+    labeled_rows = s.query(News).filter(News.label != None).all()
+    titles_known = []
+    labels_known = []
+    for row in labeled_rows:
+        titles_known.append(row.title)
+        labels_known.append(row.label)
+    model = NaiveBayesClassifier(1)
+    model.fit(titles_known, labels_known)
+
+    #ранжированная таблица
+    non_labeled_rows = s.query(News).filter(News.label == None).all()
+    titles = []
+    for row in non_labeled_rows:
+        titles.append(row.title)
+    labels = model.predict(titles)
+
+    rows = []
+    for label in ('good', 'maybe', 'never'):
+        ix = 0
+
+        for title_label in labels:
+            if title_label == label:
+                rows.append(non_labeled_rows[ix])
+            ix += 1
+
+    return template('news_recommendations', rows=rows)
 
 
 def web_server():
@@ -68,3 +111,4 @@ def web_server():
 
 if __name__ == "__main__":
     web_server()
+
